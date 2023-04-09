2a6cec0ab2eede2e6fc5de28622e16cf5be68efc
chris <gw2hicuppp@gmail.com>
2023-04-09 04:29:22 -0400

    Previous Counters + pretty + localstorage
    I added probably too much for one commit. But basically added Previous counter sections, made it look nice with css I found online and also added a super simple localstorage integration to keep counter/name/previous counters

--- counter.js
+++ counter.js
@@ -1,11 +1,22 @@
 // When button is pressed
 let counter = window.localStorage.getItem('counter') || 0;
-let counterDisplay = document.getElementById('counter-display');
+let currentName = window.localStorage.getItem('currentName') || '';
+const counterDisplay = document.getElementById('counter-display');
+const counterHeader = document.getElementById('counter-header');
+const nameEntry = document.getElementById('name-input');
+const li = document.getElementById('list');
+
 updateDisplayCounter(counter)
+updatePreviousCounters();
+
+
+if(currentName != '') {
+  document.getElementById('counter-header').innerHTML = currentName + " Counter";
+  nameEntry.value = currentName;
+}
 
 document.addEventListener('click', function(event) {
   if (event.target.id == 'increment') {
-    // Tick counter up by one
     counter++;
     window.localStorage.setItem('counter', counter);
     updateDisplayCounter(counter);
@@ -18,15 +29,47 @@ document.addEventListener('click', function(event) {
   }
 
   if (event.target.id == 'reset') {
-    counter = 0;
-    updateDisplayCounter(counter);
-    window.localStorage.setItem('counter', counter);
+    let counterNames = JSON.parse(window.localStorage.getItem('counterNames')) || [];
+    let counters = JSON.parse(window.localStorage.getItem('counters')) || [];
+    let currentName = window.localStorage.getItem('currentName');
+
+    if ((currentName != '' && currentName != null) && counter != 0) {
+      counterNames.push(currentName);
+      counters.push(counter);
+      window.localStorage.setItem('counterNames', JSON.stringify(counterNames));
+      window.localStorage.setItem('counters', JSON.stringify(counters));
+    }
+    if ((currentName == '' || currentName === null) && counter != 0) {
+      counterNames.push('Random Counter');
+      counters.push(counter);
+      window.localStorage.setItem('counterNames', JSON.stringify(counterNames));
+      window.localStorage.setItem('counters', JSON.stringify(counters));
+    }
+
+    updatePreviousCounters();
+    updateDisplayCounter(0);
+    window.localStorage.setItem('counter', 0);
+    counter = 0
   }
+
+});
+
+nameEntry.addEventListener('keyup', function(event) {
+  window.localStorage.setItem('currentName', nameEntry.value);
+  counterHeader.innerHTML = nameEntry.value + " Counter";
 });
 
 function updateDisplayCounter(counter) {
   counterDisplay.innerHTML = counter; 
 }
 
-
-// todo: clear button and undo button
+function updatePreviousCounters() {
+  let counterNames = JSON.parse(window.localStorage.getItem('counterNames')) || [];
+  let counters = JSON.parse(window.localStorage.getItem('counters')) || [];
+  li.innerHTML = '';
+  for (let i = counterNames.length; i > 0; i--) {
+    let li = document.createElement('li');
+    li.innerHTML = counterNames[i - 1] + ': ' + counters[i - 1];
+    document.getElementById('list').appendChild(li);
+  }
+}
